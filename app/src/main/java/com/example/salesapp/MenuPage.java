b42d0712450392e04f1ea2d7ee48dd9130b0a678
Gyamerah-266 <102665481+Gyamerah-266@users.noreply.github.com>
2023-07-09 18:44:27 +0000

    Update MenuPage.java

--- app/src/main/java/com/example/salesapp/MenuPage.java
+++ app/src/main/java/com/example/salesapp/MenuPage.java
@@ -15,7 +15,7 @@ public class MenuPage extends AppCompatActivity {
      ListView listView;
     private ArrayAdapter<String> adapter;
 
-    private String[] selectableItems = {"Table 1", "Table 2", "Table 3", "Table 4", "Table 5","Table 6","Table 7","Table 8","Table 9","Table 10","Table 11","Table 12","Table 13","Table 14","Table 15","Table 16","Table 17","Table 18","Table 19," };
+    private String[] selectableItems = {"Table 1", "Table 2", "Table 3", "Table 4", "Table 5","Table 6","Table 7","Table 8","Table 9","Table 10","Table 11","Table 12","Table 13","Table 14","Table 15","Table 16","Table 17","Table 18","Table 19" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
