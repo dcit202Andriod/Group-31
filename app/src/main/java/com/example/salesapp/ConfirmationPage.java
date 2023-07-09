1acf760f4650a3911a2599147cce1de39cc141db
Nanayaw60 <103117206+Nanayaw60@users.noreply.github.com>
2023-07-09 18:21:33 +0000

    ConfirmationPage.java

--- app/src/main/java/com/example/salesapp/ConfirmationPage.java
+++ app/src/main/java/com/example/salesapp/ConfirmationPage.java
@@ -14,9 +14,16 @@ public class ConfirmationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
-
-
-
+        
+         Button myButton = findViewById(R.id.SignoutButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform the action to go to the new page
+                Intent intent = new Intent(ConfirmationPage.this, Signup.class);
+                startActivity(intent);
+            }
+        });
     }
      public void openMenuPage (View v){
         startActivity(new Intent(this, MenuPage.class));
